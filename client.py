139b24d9063caa4d873c6ee5488adf71c23f58d6
jobedev95 <m95jbnb@hotmail.com>
2024-11-10 23:31:31 +0100

    Initial commit

---
+++ client.py
@@ -0,0 +1,124 @@
+import socket
+import threading
+import pyinputplus as py
+from colorama import Fore
+import time
+
+HOST = "127.0.0.1"
+PORT = 55610
+
+# Dictionary with colors for the prints
+colors: dict[str, str] = {
+    "RED": Fore.RED,
+    "GREEN": Fore.GREEN,
+    "BLUE": Fore.BLUE,
+    "YELLOW": Fore.YELLOW,
+    "WHITE": Fore.WHITE,
+    "RESET": Fore.RESET,
+}
+
+# Flag used to inform both threads if the socket has been closed
+socket_is_closed = False
+
+
+def receive_messages(client_socket, username) -> None:
+    """Receives messages sent from the server.
+    If the server sends "USERNAME_REQUEST" the client will respond with the clients username."""
+    global socket_is_closed
+
+    while True:
+        try:
+            # Receives data from the server and decodes it
+            message = client_socket.recv(1024).decode("utf-8")
+
+            # Informs client that it has successfully connected to the server
+            if message == "CLIENT_CONNECTED":
+                print(colors["GREEN"] + "Connected to the chat server. You can start chatting!" + colors["RESET"])
+                print(colors["YELLOW"] + "You can leave the chat at any time by entering 'exit'.\n" + colors["RESET"])
+
+            # Sends the username to server when requested
+            elif message == "USERNAME_REQUEST":
+                client_socket.send(username.encode("utf-8"))
+
+            # If idle for more than 120 seconds the socket will be closed
+            elif message == "IDLE_TIMEOUT":
+                print(colors["RED"] + "\nYou have been idle for too long. You have been disconnected." + colors["RESET"])
+                socket_is_closed = True
+                client_socket.close()
+                break
+
+            # Else it prints the received message in the given color
+            else:
+                # All messages from the server will contain the color and the message separated by an exclamation mark "!" (eg. 'BLUE!message')
+                # Here the color and the message itself gets separated into two before the content gets printed
+                color, content = message.split("!", 1)
+                print(colors[color] + content + colors["RESET"])
+
+        # Closes the connection if an error occurs
+        except Exception as e:
+            # If the user closed the chat the exception will be handled by breaking the loop
+            if socket_is_closed:
+                break
+
+            # All other exceptions will print the given error in red and close the socket
+            print(colors["RED"] + f"\nAn error occurred! Error message: {e}" + colors["RESET"])
+            client_socket.close()
+            break
+
+
+def send_messages(client_socket, username) -> None:
+    """Handles the clients input and sends it to the server.
+    If the client sends 'exit' it ends the loop and the client disconnects."""
+
+    global socket_is_closed
+
+    while not socket_is_closed:
+        try:
+            time.sleep(2)  # Timer ensuring that the client cannot spam the server
+            message = py.inputStr()  # Takes the message from the user
+
+            # Closes the socket if user enters 'exit'
+            if message.lower() == "exit":
+                socket_is_closed = True
+                client_socket.close()  # Closes the socket
+                break
+            else:
+                message = f"{username}: {message}"
+
+            # Sends the message to the server
+            client_socket.send(message.encode("utf-8"))
+        except Exception as e:
+            if socket_is_closed:
+                print(colors["RED"] + "Cannot send more messages. You have been disconnected." + colors["RESET"])
+                break
+            print(f"Error when attempting to send the message. Error message: {e}")
+            break
+
+
+def start_client() -> None:
+    """Asks the user for a username and connects to the main server."""
+
+    # Gets the username and ensures that it only contains letters A-Z
+    username = py.inputRegex(
+        prompt="Enter you username: ",
+        regex=r"^[A-Za-z]+$",
+    ).upper()
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        client_socket.connect((HOST, PORT))  # Connects to the server
+
+        # Creates and starts a thread to receive messages from the server
+        receive_thread = threading.Thread(target=receive_messages, args=(client_socket, username))
+        receive_thread.start()
+
+        # Creates and starts a thread to send messages to the server
+        send_thread = threading.Thread(target=send_messages, args=(client_socket, username))
+        send_thread.start()
+
+        # Waits for both threads to finish before the main thread continues
+        send_thread.join()
+        receive_thread.join()
+
+
+if __name__ == "__main__":
+    start_client()
---
+++ server.py
@@ -0,0 +1,149 @@
+import socket
+import threading
+import time
+from typing import Any
+
+HOST = "127.0.0.1"
+PORT = 55610
+
+# List stores all clients connected to the server
+clients = []
+
+# Dictionary that logs the time of latest activity by a client
+latest_activity = {}
+
+# Threading lock used for safer handling of the 'clients' list and 'latest_activity' dict
+clients_lock = threading.Lock()
+
+# List stores all client usernames
+usernames: list[str] = []
+
+
+def broadcast(message, exclude_socket=None) -> None:
+    """Broadcasts message to all connected clients.
+    The 'exclude_socket'-parameter allows for excluding a client socket from the broadcast."""
+
+    with clients_lock:
+        print(f"Broadcasting message: {message.decode("utf-8")}")
+        # Loops through all connected clients
+        for client in clients:
+            # Attempts to send the message to all but the excluded client (if one is specified)
+            if client != exclude_socket:
+                try:
+                    client.send(message)
+                    print(f"Sent {message} to {client}")
+                except BrokenPipeError:
+                    remove_client(client)
+
+
+def start_server() -> None:
+    """Starts the server and initiates incoming connections."""
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        server_socket.bind((HOST, PORT))  # Binds the server to local host
+        server_socket.listen()  # Pauses all execution until incoming requests from clients are received
+        print(f"Server listening on socket address {HOST}:{PORT}")
+
+        # Creates and starts the idle time thread for each socket that keeps track of all clients idle times
+        idle_time_thread = threading.Thread(target=check_idle_time)
+        idle_time_thread.start()
+
+        while True:
+            # Returns a tuple containing client socket and address
+            client_socket, client_addr = server_socket.accept()
+            print(f"Connected with {str(client_addr)}")
+
+            client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+
+            # Retrieves username from client
+            client_socket.send("USERNAME_REQUEST".encode("utf-8"))
+            username = client_socket.recv(1024).decode("utf-8")
+            usernames.append(username)
+
+            # Safely adds the client to the 'clients' list and logs the time of this activity
+            with clients_lock:
+                clients.append(client_socket)
+                latest_activity[client_socket] = time.time()
+
+            # Informs the client that it has successfully connected to the server
+            client_socket.send("CLIENT_CONNECTED".encode("utf-8"))
+
+            # Broadcasts that a new user has entered the chat in green color
+            broadcast(f"GREEN!\n'{username}' just entered the chat!\n".encode("utf-8"), exclude_socket=client_socket)
+
+            # Creates and starts a new thread that handles each client connection
+            thread = threading.Thread(target=handle_client, args=(client_socket,))
+            thread.start()
+
+
+def handle_client(client_socket) -> None:
+    """Handles the connection with a client. Removes client when cleanly disconnected."""
+    while True:
+        try:
+            # Receives and decodes message from client
+            message = client_socket.recv(1024).decode("utf-8")
+
+            # Logs the time of the latest message sent by the client
+            with clients_lock:
+                latest_activity[client_socket] = time.time()
+
+            # Removes the client if it has cleanly disconnected
+            if not message:
+                remove_client(client_socket)
+                break
+
+            # Sends received message to all other clients in blue color
+            broadcast(f"BLUE!{message}".encode("utf-8"), exclude_socket=client_socket)
+
+        except Exception as e:
+            print(f"Something went wrong with the connection. Error message: {e}")
+            remove_client(client_socket)
+            break
+
+
+def check_idle_time() -> None:
+    """Checks every five seconds to see if a client has been idle for more than two minutes.
+    Disconnects the client if that is the case."""
+    while True:
+        time.sleep(5)
+
+        # Will store any potential idle clients that will be removed
+        clients_to_remove = []
+
+        with clients_lock:
+            # Loops through all clients to check their idle times
+            for client in clients:
+                # Calculates the idle time
+                idle_time = time.time() - latest_activity[client]
+
+                # Appends client to removal list if idle for more than two minutes
+                if idle_time > 7:
+                    client.send("IDLE_TIMEOUT".encode("utf-8"))
+                    clients_to_remove.append(client)
+
+        # Removes all idle clients
+        for client in clients_to_remove:
+            remove_client(client)
+
+
+def remove_client(client_socket) -> None:
+    """Removes a client and its username from the 'clients' and 'usernames' lists."""
+    with clients_lock:
+        if client_socket in clients:
+            try:
+                client_index = clients.index(client_socket)  # Finds index of client
+                username = usernames[client_index]  # Gets the username
+
+                clients.remove(client_socket)  # Removes client from 'clients' list
+                usernames.remove(username)  # Removes the username from 'users' list
+                del latest_activity[client_socket]  # Removes the idle time logging data from 'latest_activity' dict
+                client_socket.close()  # Closes the socket
+
+                # Informs all other clients that the user has disconnected from the server in red color
+                print(f"Broadcasting message: 'User '{username}' just left the chat!'")
+                # broadcast(f"RED!\n'{username}' just left the chat!\n".encode("utf-8"))
+            except ValueError:
+                print("Client already removed")
+
+
+if __name__ == "__main__":
+    start_server()
